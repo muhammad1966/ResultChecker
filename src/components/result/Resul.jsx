7438a4393fd4469a3b85968574c948268d2bbb48
Muhammad <bsmuhammad352@gmail.com>
2024-05-31 02:28:43 +0300

    fri-31st-5-2024-1

--- src/components/result/Resul.jsx
+++ src/components/result/Resul.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import './result.css'
+import {useForm} from 'react-hook-form'
 
 export const Resul = () => {
+    const {register, handleSubmit} = useForm();
+
+    const onSubmit = (data) =>{
+        let Data = data;
+    }
   return (
     <div className='result-div'>
-
+        <div className='result-content-div'>
+            <h3>
+                Provide your name or student number in the field below.
+            </h3>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input className='result-input' type="text"  {...register("identity")}/>
+                <button className='result-submit' type="submit">Submit</button>
+            </form>
+        </div>
     </div>
+
+
   )
 }
+
+
