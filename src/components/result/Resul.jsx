d7a3677c103c5d244d392542667455bcb92dae04
Muhammad <bsmuhammad352@gmail.com>
2024-12-03 16:06:18 +0200

    3-12-24

--- src/components/result/Resul.jsx
+++ src/components/result/Resul.jsx
@@ -1,23 +1,44 @@
 import React from 'react'
 import './result.css'
-import {useForm} from 'react-hook-form'
+import {useState} from 'react'
 
 export const Resul = () => {
-    const {register, handleSubmit} = useForm();
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-    const onSubmit = (data) =>{
-        let Data = data;
+    const BASE_URL = "https://jsonplaceholder.typicode.com/posts";
+
+    const fetchData = () => {
+        setError(null);
+        setLoading(true);
+
+        fetch(BASE_URL)
+        .then((response) =>{
+            if(!response.ok){
+                throw new Error ("Something went wrong with the networt")
+            }
+            return response.json();
+        })
+        .then((data) =>{
+            setData(data);
+        })
+        .catch((error)=>{
+            setError(error);
+        })
+        .finally(()=>{
+            setLoading(false);
+        })
     }
+    
   return (
     <div className='result-div'>
         <div className='result-content-div'>
             <h3>
                 Provide your name or student number in the field below.
             </h3>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <input className='result-input' type="text"  {...register("identity")}/>
-                <button className='result-submit' type="submit">Submit</button>
-            </form>
+            <input className='result-input' type="text"/>
+            <button className='result-submit' type="submit" onClick={fetchData}>Submit</button>
         </div>
     </div>
 
