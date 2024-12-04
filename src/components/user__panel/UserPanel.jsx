d5d3a057133afd8351fca4060efd5c06587018a5
Muhammad <bsmuhammad352@gmail.com>
2024-12-04 20:06:18 +0200

    4-12-24 2

--- src/components/user__panel/UserPanel.jsx
+++ src/components/user__panel/UserPanel.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import "./userPanel.css"
 
 export const UserPanel = ({data}) => {
   return (
-    <div>UserPanel</div>
+    <div className='user-panel-div'>
+      <div className='user-panel-content'>
+        <div className='content-numeric'>
+          11111
+        </div>
+        <div className='content-chat'>
+          222222
+        </div>
+      </div>
+    </div>
   )
 }
